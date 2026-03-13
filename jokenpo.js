e947ff2183e89c0bf9ce11137f0aad31d8941e64
Renildo226 <renildogomes2005@gmail.com>
2026-03-13 14:39:28 +0100

    update

--- jokenpo.js
+++ jokenpo.js
@@ -11,17 +11,17 @@
         let ale = Math.floor(Math.random() * 3) + 1
         if(ale==1) {
             res.innerHTML="EMPATE"
-            if (ale == 1) {comOption = `Pedra <img src="./imagens/pedra.png" alt="" class="objecto"></img>`}
-            if(playOption == 1) {playOption = `Pedra <img src="../imagens/pedra.png" alt="" class="objecto"></img>`}
+            if (ale == 1) {comOption = `Pedra <img src="imagens/pedra.png" alt="" class="objecto"></img>`}
+            if(playOption == 1) {playOption = `Pedra <img src="imagens/pedra.png" alt="" class="objecto"></img>`}
 
         }else if(ale==2) {
             res.innerHTML="Computador Ganhou!"
-            if(playOption == 1) {playOption = `Pedra <img src="../imagens/pedra.png" alt="" class="objecto"></img>`}
-            if (ale == 2) {comOption = `Papel <img src="../imagens/papel.png" alt="" class="objecto"></img>`}
+            if(playOption == 1) {playOption = `Pedra <img src="imagens/pedra.png" alt="" class="objecto"></img>`}
+            if (ale == 2) {comOption = `Papel <img src="imagens/papel.png" alt="" class="objecto"></img>`}
 
         }else if(ale==3){
-            if(playOption == 1) {playOption = `Pedra <img src="../imagens/pedra.png" alt="" class="objecto"></img>`}
-            if (ale == 3) {comOption = `Tesoura <img src="../imagens/tesouras.png" alt="" class="objecto"></img>`}
+            if(playOption == 1) {playOption = `Pedra <img src="imagens/pedra.png" alt="" class="objecto"></img>`}
+            if (ale == 3) {comOption = `Tesoura <img src="imagens/tesouras.png" alt="" class="objecto"></img>`}
             res.innerHTML="Jogador Ganhou!"
         }
         res1.innerHTML = `<p>computador jogou ${comOption} </p>`
@@ -32,16 +32,16 @@
         let playOption = valor
         let ale = Math.floor(Math.random() * 3) + 1
         if(ale==1) {
-            if(playOption == 2) {playOption = `Papel <img src="../imagens/papel.png" alt="" class="objecto"></img>`}
-            if (ale == 1) {comOption = `Pedra <img src="../imagens/pedra.png" alt="" class="objecto"></img>`}
+            if(playOption == 2) {playOption = `Papel <img src="imagens/papel.png" alt="" class="objecto"></img>`}
+            if (ale == 1) {comOption = `Pedra <img src="imagens/pedra.png" alt="" class="objecto"></img>`}
             res.innerHTML="Jogador Ganhou!"
         }else if(ale==2) {
-            if(playOption == 2) {playOption = `Papel <img src="../imagens/papel.png" alt="" class="objecto"></img>`}
-            if (ale == 2) {comOption = `Papel <img src="../imagens/papel.png" alt="" class="objecto"></img>`}
+            if(playOption == 2) {playOption = `Papel <img src="imagens/papel.png" alt="" class="objecto"></img>`}
+            if (ale == 2) {comOption = `Papel <img src="imagens/papel.png" alt="" class="objecto"></img>`}
             res.innerHTML="EMPATE"
         }else if(ale==3) {
-            if(playOption == 2) {playOption = `Papel <img src="../imagens/papel.png" alt="" class="objecto"></img>`}
-            if (ale == 3) {comOption = `Tesoura <img src="../imagens/tesouras.png" alt="" class="objecto"></img>`}
+            if(playOption == 2) {playOption = `Papel <img src="imagens/papel.png" alt="" class="objecto"></img>`}
+            if (ale == 3) {comOption = `Tesoura <img src="imagens/tesouras.png" alt="" class="objecto"></img>`}
             res.innerHTML="Computador Ganhou!"
         }
         res1.innerHTML = `<p>computador jogou ${comOption} </p>`
@@ -52,16 +52,16 @@
         let playOption=valor
         let ale = Math.floor(Math.random() * 3) + 1
         if(ale==1) {
-            if(playOption == 3) {playOption = `Tesoura <img src="../imagens/tesouras.png" alt="" class="objecto"></img>`}
-            if (ale == 1) {comOption = `Pedra <img src="../imagens/pedra.png" alt="" class="objecto"></img>`}
+            if(playOption == 3) {playOption = `Tesoura <img src="imagens/tesouras.png" alt="" class="objecto"></img>`}
+            if (ale == 1) {comOption = `Pedra <img src="imagens/pedra.png" alt="" class="objecto"></img>`}
             res.innerHTML="Computador Ganhou!"
         }else if(ale==2) {
-            if(playOption == 3) {playOption = `Tesoura <img src="../imagens/tesouras.png" alt="" class="objecto"></img>`}
-            if (ale == 2) {comOption = `Papel <img src="../imagens/papel.png" alt="" class="objecto"></img>`}
+            if(playOption == 3) {playOption = `Tesoura <img src="imagens/tesouras.png" alt="" class="objecto"></img>`}
+            if (ale == 2) {comOption = `Papel <img src="imagens/papel.png" alt="" class="objecto"></img>`}
             res.innerHTML="Jogador Ganhou!"
         }else if(ale==3) {
-            if(playOption == 3) {playOption = `Tesoura <img src="../imagens/tesouras.png" alt="" class="objecto"></img>`}
-            if (ale == 3) {comOption = `Tesoura <img src="../imagens/tesouras.png" alt="" class="objecto"></img>`}
+            if(playOption == 3) {playOption = `Tesoura <img src="imagens/tesouras.png" alt="" class="objecto"></img>`}
+            if (ale == 3) {comOption = `Tesoura <img src="imagens/tesouras.png" alt="" class="objecto"></img>`}
             res.innerHTML="EMPATE"
         }
         res1.innerHTML = `<p>computador jogou ${comOption} </p>`
